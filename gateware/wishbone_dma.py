8c56f3b67ec12b4c678e9ea026aa1ddc9d3eb018
Florent Kermarrec <florent@enjoy-digital.fr>
2024-06-26 15:52:02 +0200

    gateware/wishbone_dma: Cleanup pass.

--- gateware/wishbone_dma.py
+++ gateware/wishbone_dma.py
@@ -16,6 +16,12 @@ from litex.soc.cores.dma import *
 
 from litepcie.frontend.dma import *
 
+# DMA Layouts --------------------------------------------------------------------------------------
+
+def dma_descriptor_layout():
+    layout = [("host_addr", 32), ("bus_addr",32), ("length",  32)]
+    return EndpointDescription(layout)
+
 # WishboneDMAReaderCtrl ----------------------------------------------------------------------------
 
 class WishboneDMAReaderCtrl(WishboneDMAReader):
@@ -153,20 +159,17 @@ class WishboneDMAWriterCtrl(WishboneDMAWriter):
         )
         fsm.act("DONE", self.done.eq(1))
 
-# DMA Layouts --------------------------------------------------------------------------------------
 
-def dma_descriptor_layout():
-    layout = [("host_addr", 32), ("bus_addr",32), ("length",  32)]
-    return EndpointDescription(layout)
+# LiteWishbone2PCIeDMANative -----------------------------------------------------------------------
 
-class LiteWishbone2PCIeDMANative(Module, AutoCSR):
+class LiteWishbone2PCIeDMANative(LiteXModule):
     def __init__(self, endpoint, dma_wr, data_width=32):
 
         dma_wr_desc = stream.Endpoint(descriptor_layout())
-        self.submodules.dma_fifo = dma_fifo = stream.SyncFIFO(descriptor_layout(), 16)
+        self.dma_fifo = dma_fifo = stream.SyncFIFO(descriptor_layout(), 16)
 
         desc_wr = stream.Endpoint(dma_descriptor_layout())
-        self.submodules.fifo_wr = fifo_wr = stream.SyncFIFO(dma_descriptor_layout(), 16)
+        self.fifo_wr = fifo_wr = stream.SyncFIFO(dma_descriptor_layout(), 16)
 
         self.host_addr = host_addr = Signal(32, reset=0)
         self.length = length = Signal(32, reset=0)
@@ -176,15 +179,11 @@ class LiteWishbone2PCIeDMANative(Module, AutoCSR):
         self.ready = Signal(reset=0)
 
         self.bus_wr = wishbone.Interface(data_width=data_width)
-        self.submodules.wb_dma = wb_dma = WishboneDMAReaderCtrl(self.bus_wr)
+        self.wb_dma = wb_dma = WishboneDMAReaderCtrl(self.bus_wr)
         wb_dma.add_ctrl()
-        self.submodules.conv_wr = conv_wr = stream.Converter(nbits_from=data_width, nbits_to=endpoint.phy.data_width)
+        self.conv_wr = conv_wr = stream.Converter(nbits_from=data_width, nbits_to=endpoint.phy.data_width)
         self.irq = Signal(reset=0)
         dma_enable = Signal(reset=0)
-
-        self.test1 = CSRStatus(32, reset=0)
-        self.test2 = CSRStatus(32, reset=0)
-        self.test3 = CSRStatus(32, reset=0)
 
         self.comb += [
             wb_dma.enable.eq(dma_enable),
@@ -208,37 +207,34 @@ class LiteWishbone2PCIeDMANative(Module, AutoCSR):
 
         ctrl_fsm = FSM(reset_state="IDLE")
         ctrl_fsm = ResetInserter()(ctrl_fsm)
-        self.submodules.ctrl_fsm = ctrl_fsm
+        self.ctrl_fsm = ctrl_fsm
         ctrl_fsm.act("IDLE",
-                     If(fifo_wr.source.valid & dma_wr_desc.ready,
-                        NextState("RUN"),
-                        NextValue(self.test1.status, self.test1.status + 1),
-                        dma_wr_desc.valid.eq(1),
-                        NextValue(dma_enable, 1),
-                        )
-                     )
+            If(fifo_wr.source.valid & dma_wr_desc.ready,
+                NextState("RUN"),
+                dma_wr_desc.valid.eq(1),
+                NextValue(dma_enable, 1),
+            )
+        )
         ctrl_fsm.act("RUN",
-                     NextValue(self.test2.status, self.test2.status + 1),
-                     If(wb_dma.done,
-                        fifo_wr.source.ready.eq(1),
-                        NextValue(self.test3.status, self.test3.status + 1),
-                        NextState("IDLE"),
-                        NextValue(dma_enable, 0),
-                        self.irq.eq(~irq_disable.storage),
-                        self.ready.eq(1)
-                        )
-                     )
+            If(wb_dma.done,
+                fifo_wr.source.ready.eq(1),
+                NextState("IDLE"),
+                NextValue(dma_enable, 0),
+                self.irq.eq(~irq_disable.storage),
+                self.ready.eq(1)
+            )
+        )
 
 
-# LitePCIe2WishboneDMA --------------------------------------------------------------------------------
+# LitePCIe2WishboneDMANative -----------------------------------------------------------------------
 
-class LitePCIe2WishboneDMANative(Module, AutoCSR):
+class LitePCIe2WishboneDMANative(LiteXModule):
     def __init__(self, endpoint, dma_rd, data_width=32):
 
         dma_rd_desc = stream.Endpoint(descriptor_layout())
-        self.submodules.dma_fifo = dma_fifo = stream.SyncFIFO(descriptor_layout(), 1)
+        self.dma_fifo = dma_fifo = stream.SyncFIFO(descriptor_layout(), 1)
         desc_rd = stream.Endpoint(dma_descriptor_layout())
-        self.submodules.fifo_rd = fifo_rd = stream.SyncFIFO(dma_descriptor_layout(), 16)
+        self.fifo_rd = fifo_rd = stream.SyncFIFO(dma_descriptor_layout(), 16)
 
         self.host_addr = host_addr = Signal(32, reset=0)
         self.length = length = Signal(32, reset=0)
@@ -248,15 +244,11 @@ class LitePCIe2WishboneDMANative(Module, AutoCSR):
         self.ready = Signal(reset=0)
 
         self.bus_rd = wishbone.Interface(data_width=data_width)
-        self.submodules.wb_dma = wb_dma = WishboneDMAWriterCtrl(self.bus_rd)
+        self.wb_dma = wb_dma = WishboneDMAWriterCtrl(self.bus_rd)
         wb_dma.add_ctrl()
         self.irq = Signal(reset=0)
-        self.submodules.conv_rd = conv_rd = stream.Converter(nbits_from=endpoint.phy.data_width, nbits_to=data_width)
+        self.conv_rd = conv_rd = stream.Converter(nbits_from=endpoint.phy.data_width, nbits_to=data_width)
         dma_enable = Signal(reset=0)
-
-        self.test1 = CSRStatus(32, reset=0)
-        self.test2 = CSRStatus(32, reset=0)
-        self.test3 = CSRStatus(32, reset=0)
 
         self.comb += [
             wb_dma.enable.eq(dma_enable),
@@ -280,23 +272,20 @@ class LitePCIe2WishboneDMANative(Module, AutoCSR):
 
         ctrl_fsm = FSM(reset_state="IDLE")
         ctrl_fsm = ResetInserter()(ctrl_fsm)
-        self.submodules.ctrl_fsm = ctrl_fsm
+        self.ctrl_fsm = ctrl_fsm
         ctrl_fsm.act("IDLE",
-                     If(fifo_rd.source.valid & dma_rd_desc.ready,
-                        NextState("RUN"),
-                        NextValue(self.test1.status, self.test1.status + 1),
-                        dma_rd_desc.valid.eq(1),
-                        NextValue(dma_enable, 1),
-                        )
-                     )
+            If(fifo_rd.source.valid & dma_rd_desc.ready,
+                NextState("RUN"),
+                dma_rd_desc.valid.eq(1),
+                NextValue(dma_enable, 1),
+            )
+        )
         ctrl_fsm.act("RUN",
-                     NextValue(self.test2.status, self.test2.status + 1),
-                     If(wb_dma.done,
-                        fifo_rd.source.ready.eq(1),
-                        NextValue(self.test3.status, self.test3.status + 1),
-                        NextState("IDLE"),
-                        NextValue(dma_enable, 0),
-                        self.irq.eq(~irq_disable.storage),
-                        self.ready.eq(1),
-                    )
+            If(wb_dma.done,
+                fifo_rd.source.ready.eq(1),
+                NextState("IDLE"),
+                NextValue(dma_enable, 0),
+                self.irq.eq(~irq_disable.storage),
+                self.ready.eq(1),
+            )
         )
