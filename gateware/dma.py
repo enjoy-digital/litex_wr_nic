0809066b792f72bbad6975bcfc68f44ecf786055
Florent Kermarrec <florent@enjoy-digital.fr>
2024-06-27 09:19:24 +0200

    gateware/dma: Simplify a bit.

--- gateware/dma.py
+++ gateware/dma.py
@@ -10,8 +10,6 @@ from litex.gen import *
 
 from litex.soc.interconnect import wishbone
 
-from litepcie.common import *
-
 from litex.soc.cores.dma import *
 
 from litepcie.frontend.dma import *
@@ -19,7 +17,11 @@ from litepcie.frontend.dma import *
 # DMA Layouts --------------------------------------------------------------------------------------
 
 def dma_descriptor_layout():
-    layout = [("host_addr", 32), ("bus_addr",32), ("length",  32)]
+    layout = [
+        ("host_addr", 32),
+        ("bus_addr" , 32),
+        ("length"   , 32),
+    ]
     return EndpointDescription(layout)
 
 # LitePCIe2WishboneDMA -----------------------------------------------------------------------------
@@ -28,10 +30,13 @@ class LitePCIe2WishboneDMA(LiteXModule):
     def __init__(self, endpoint, dma, data_width=32, mode="pcie2wb"):
         assert mode in ["pcie2wb", "wb2pcie"]
 
+        self.bus = wishbone.Interface(data_width=data_width)
+
         dma_desc = stream.Endpoint(descriptor_layout())
-        self.dma_fifo = dma_fifo = stream.SyncFIFO(descriptor_layout(), 1)
-        desc = stream.Endpoint(dma_descriptor_layout())
-        self.fifo = fifo = stream.SyncFIFO(dma_descriptor_layout(), 16)
+        desc     = stream.Endpoint(dma_descriptor_layout())
+        dma_fifo = stream.SyncFIFO(descriptor_layout(), 1)
+        fifo = stream.SyncFIFO(dma_descriptor_layout(), 16)
+        self.submodules += dma_fifo, fifo
 
         self.host_addr   = host_addr   = Signal(32)
         self.length      = length      = Signal(32)
@@ -40,7 +45,6 @@ class LitePCIe2WishboneDMA(LiteXModule):
         self.start       = start       = Signal(1)
         self.ready       = Signal(reset=0)
 
-        self.bus = wishbone.Interface(data_width=data_width)
         if mode == "pcie2wb":
             self.wb_dma = wb_dma = WishboneDMAWriter(self.bus, endianness="big")
             self.conv   = conv   = stream.Converter(nbits_from=endpoint.phy.data_width, nbits_to=data_width)
@@ -49,8 +53,6 @@ class LitePCIe2WishboneDMA(LiteXModule):
             self.conv   = conv   = stream.Converter(nbits_from=data_width, nbits_to=endpoint.phy.data_width)
         wb_dma.add_ctrl()
         self.irq = Signal()
-
-        dma_enable = Signal()
 
         if mode == "pcie2wb":
             self.comb += [
@@ -64,7 +66,6 @@ class LitePCIe2WishboneDMA(LiteXModule):
             ]
 
         self.comb += [
-            wb_dma.enable.eq(dma_enable),
             dma_desc.connect(dma_fifo.sink),
             dma_fifo.source.connect(dma.desc_sink),
             desc.connect(fifo.sink),
@@ -80,20 +81,20 @@ class LitePCIe2WishboneDMA(LiteXModule):
             dma_desc.length.eq(fifo.source.length),
         ]
 
-        self.fsm = fsm = ResetInserter()(FSM(reset_state="IDLE"))
+        self.fsm = fsm = FSM(reset_state="IDLE")
         fsm.act("IDLE",
+            wb_dma.enable.eq(0),
             If(fifo.source.valid & dma_desc.ready,
                 NextState("RUN"),
                 dma_desc.valid.eq(1),
-                NextValue(dma_enable, 1),
             )
         )
         fsm.act("RUN",
+            wb_dma.enable.eq(1),
             If(wb_dma.done,
                 fifo.source.ready.eq(1),
-                NextState("IDLE"),
-                NextValue(dma_enable, 0),
                 self.irq.eq(~irq_disable.storage),
                 self.ready.eq(1),
+                NextState("IDLE"),
             )
         )
