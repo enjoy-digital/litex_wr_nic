8b1385098c3440b3723b703b18673de93d6d19fb
Florent Kermarrec <florent@enjoy-digital.fr>
2024-07-30 11:22:28 +0200

    gateware: Move WR cores files to wr_core and update targets.

--- spec_a7_wr_nic.py
+++ spec_a7_wr_nic.py
@@ -431,12 +431,14 @@ class BaseSoC(SoCCore):
 
     def add_sources(self):
         custom_files = [
-            "gateware/xwrc_platform_vivado.vhd",
-            "gateware/xwrc_board_artix7.vhd",
-            "gateware/xwrc_board_artix7_wrapper.vhd",
+            # 7-Series GTP PHY.
             "gateware/wr_phy/whiterabbit_gtpe2_channel_wrapper.vhd",
             "gateware/wr_phy/whiterabbit_gtpe2_channel_wrapper_gt.vhd",
             "gateware/wr_phy/wr_gtp_phy_family7.vhd",
+            # 7-Series Core.
+            "gateware/wr_core/xwrc_platform_vivado.vhd",
+            "gateware/wr_core/xwrc_board_artix7.vhd",
+            "gateware/wr_core/xwrc_board_artix7_wrapper.vhd",
         ]
 
         for cf in custom_files:
