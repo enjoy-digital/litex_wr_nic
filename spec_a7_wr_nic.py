4a19a0a4f7022d7ea162d4962d4e9a74e0ed88c2
Florent Kermarrec <florent@enjoy-digital.fr>
2024-09-18 17:40:36 +0200

    spec_a7_wr_nic: Only keep 62.5MHZ Clk input now that issue is understood.

--- spec_a7_wr_nic.py
+++ spec_a7_wr_nic.py
@@ -47,7 +47,7 @@ from gateware.wrf_wb2stream import Wishbone2Stream
 # CRG ----------------------------------------------------------------------------------------------
 
 class _CRG(LiteXModule):
-    def __init__(self, platform, sys_clk_freq, with_white_rabbit=True, with_pcie=True, use_cfgm_clk=False):
+    def __init__(self, platform, sys_clk_freq, with_white_rabbit=True, with_pcie=True):
         self.rst            = Signal()
         self.cd_sys         = ClockDomain()
         self.cd_refclk_pcie = ClockDomain()
@@ -62,33 +62,12 @@ class _CRG(LiteXModule):
         # # #
 
         # Clk/Rst.
-        if use_cfgm_clk:
-            # CFGM Clk ~65MHz.
-            cfgm_clk      = Signal()
-            cfgm_clk_freq = int(65e6)
-            self.specials += Instance("STARTUPE2",
-                i_CLK       = 0,
-                i_GSR       = 0,
-                i_GTS       = 0,
-                i_KEYCLEARB = 1,
-                i_PACK      = 0,
-                i_USRCCLKO  = cfgm_clk,
-                i_USRCCLKTS = 0,
-                i_USRDONEO  = 1,
-                i_USRDONETS = 1,
-                o_CFGMCLK   = cfgm_clk
-            )
-            platform.add_period_constraint(cfgm_clk, 1e9/65e6)
-        else:
-            clk62p5 = platform.request("clk62p5")
+        clk62p5 = platform.request("clk62p5")
 
         # PLL.
         self. pll = pll = S7PLL(speedgrade=-3)
         self.comb += pll.reset.eq(self.rst)
-        if use_cfgm_clk:
-            pll.register_clkin(cfgm_clk, 65e6)
-        else:
-            pll.register_clkin(clk62p5, 62.5e6)
+        pll.register_clkin(clk62p5, 62.5e6)
         pll.create_clkout(self.cd_sys, sys_clk_freq)
         if with_white_rabbit:
             pll.create_clkout(self.cd_clk_125m_gtp,  125e6, margin=0)
