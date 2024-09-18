8bbe2a76ccacda31080e7c683b8c83ebf3f40cb1
Florent Kermarrec <florent@enjoy-digital.fr>
2024-09-18 11:39:29 +0200

    acorn_wr_nic: Try to connect CPU BRAM to LiteX generated BRAM (building but not yet working).

---
+++ gateware/ext_ram_tap.v
@@ -0,0 +1,13 @@
+module ext_ram_tap (
+    (* mark_debug = "true" *)
+    input  wire [31:0]  ext_ram_i_adr,
+    (* mark_debug = "true" *)
+    output wire [31:0]  ext_ram_i_dat_r,
+
+    output wire [31:0]  ext_ram_o_adr,
+    input  wire [31:0]  ext_ram_o_dat_r
+);
+
+    assign ext_ram_o_adr   = ext_ram_i_adr;
+    assign ext_ram_i_dat_r = ext_ram_o_dat_r;
+endmodule
