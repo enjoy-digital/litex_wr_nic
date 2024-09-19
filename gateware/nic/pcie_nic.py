fec3e6c6dbede8f8f46537221a0a368df2bad034
Florent Kermarrec <florent@enjoy-digital.fr>
2024-09-19 16:19:49 +0200

    gateware/nic/pcie_nic: Cleanup pass, remove unused constants.

--- gateware/nic/pcie_nic.py
+++ gateware/nic/pcie_nic.py
@@ -39,7 +39,8 @@ class PCIeNICSoC(SoCMini):
     }
 
     def add_pcie_nic(self, pcie_phy=None, eth_phy=None, ntxslots=4, nrxslots=4):
-        # MAC.
+        # Ethernet MAC.
+        # -------------
         self.add_ethernet(
             name       = "ethmac",
             phy        = eth_phy,
@@ -48,12 +49,11 @@ class PCIeNICSoC(SoCMini):
             nrxslots   = ntxslots,
             ntxslots   = nrxslots,
         )
-        self.add_constant("ETHMAC_RX_WAIT_OFFSET",  0) # CHECKME: See purpose in software.
-        self.add_constant("ETHMAC_TX_READY_OFFSET", 1) # CHECKME: See purpose in software.
-        del self.bus.slaves["ethmac_tx"]
-        del self.bus.slaves["ethmac_rx"]
+        del self.bus.slaves["ethmac_tx"] # Remove from SoC bus since directly connected to PCIe.
+        del self.bus.slaves["ethmac_rx"] # Remove from SoC bus since directly connected to PCIe.
 
-        # PCIe
+        # PCIe Core.
+        # ----------
         self.add_pcie(name="pcie", phy=pcie_phy,
             ndmas                = 1,
             max_pending_requests = 4,
@@ -61,18 +61,18 @@ class PCIeNICSoC(SoCMini):
             with_dma_buffering   = False,
             with_dma_loopback    = False,
             with_dma_table       = False,
+            with_ptm             = False,
             with_msi             = True,
             msis                 = {
                 "ETHRX" : self.ethmac.interface.sram.rx_pcie_irq,
                 "ETHTX" : self.ethmac.interface.sram.tx_pcie_irq,
             },
-            with_ptm             = False,
         )
 
         align_bits = log2_int(512)
 
-        # RX: Wishbone -> PCIe.
-        # ---------------------
+        # RX Datapath: Ethernet (RX) -> PCIe -> Host.
+        # -------------------------------------------
         self.pcie_wb2pcie_dma = pcie_wb2pcie_dma = LitePCIe2WishboneDMA(
             endpoint   = self.pcie_endpoint,
             dma        = self.pcie_dma0.writer,
@@ -91,8 +91,8 @@ class PCIeNICSoC(SoCMini):
             slave  = self.ethmac.bus_rx,
         )
 
-        # TX: PCIe -> Wishbone.
-        # ---------------------
+        # TX Datapath: Host -> PCIe -> Ethernet (TX).
+        # -------------------------------------------
         self.pcie_pcie2wb_dma = pcie_pcie2wb_dma = LitePCIe2WishboneDMA(
             endpoint   = self.pcie_endpoint,
             dma        = self.pcie_dma0.reader,
