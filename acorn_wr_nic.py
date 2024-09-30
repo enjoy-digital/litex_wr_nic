5fe5e84852525bf852d50a8863ea25a20a93f21b
Florent Kermarrec <florent@enjoy-digital.fr>
2024-09-30 19:44:40 +0200

    acorn_wr_nic: Now working.

--- acorn_wr_nic.py
+++ acorn_wr_nic.py
@@ -112,7 +112,7 @@ class BaseSoC(PCIeNICSoC):
         with_white_rabbit_ext_ram = False,
 
         # PCIe NIC.
-        with_pcie_nic = False,
+        with_pcie_nic = True,
     ):
         # Platform ---------------------------------------------------------------------------------
         platform = Platform()
@@ -461,23 +461,23 @@ class BaseSoC(PCIeNICSoC):
 
             self.ethphy = LiteEthPHYWRGMII()
 
-            # Analyzer -----------------------------------------------------------------------------
-            analyzer_signals = [
-                wrf_stream2wb.bus,
-                wrf_stream2wb.sink,
-                wrf_stream2wb.fsm,
-
-                wrf_wb2stream.bus,
-                wrf_wb2stream.source,
-                wrf_wb2stream.fsm,
-            ]
-            self.analyzer = LiteScopeAnalyzer(analyzer_signals,
-                depth        = 256,
-                clock_domain = "wr",
-                samplerate   = int(62.5e6),
-                register     = True,
-                csr_csv      = "analyzer.csv"
-            )
+#            # Analyzer -----------------------------------------------------------------------------
+#            analyzer_signals = [
+#                wrf_stream2wb.bus,
+#                wrf_stream2wb.sink,
+#                wrf_stream2wb.fsm,
+#
+#                wrf_wb2stream.bus,
+#                wrf_wb2stream.source,
+#                wrf_wb2stream.fsm,
+#            ]
+#            self.analyzer = LiteScopeAnalyzer(analyzer_signals,
+#                depth        = 256,
+#                clock_domain = "wr",
+#                samplerate   = int(62.5e6),
+#                register     = True,
+#                csr_csv      = "analyzer.csv"
+#            )
 
             if not with_pcie_nic:
                 self.add_etherbone(phy=self.ethphy, data_width=8, with_timing_constraints=False)
