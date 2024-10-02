053c7e3424d28e713593eed0944b106d6d5c8055
Florent Kermarrec <florent@enjoy-digital.fr>
2024-10-02 15:24:34 +0200

    software/kernel: Add back LiteUART driver from LitePCIe since can be useful for wrc over PCIe and similarity with LitePCIe driver.

--- software/kernel/init.sh
+++ software/kernel/init.sh
@@ -8,6 +8,12 @@ if [ "$FOUND" != "" ] ; then
     exit 0
 fi
 
+# Automatically remove liteuart module if installed.
+FOUND=$(lsmod | grep liteuart)
+if [ "$FOUND" != "" ] ; then
+    rmmod liteuart.ko
+fi
+
 # Install litepcie module.
 INS=$(insmod litepcie.ko 2>&1)
 if [ "$?" != "0" ] ; then
@@ -38,3 +44,12 @@ if [ "$?" != "0" ] ; then
         exit 1
     esac
 fi
+
+# Install liteuart module.
+insmod liteuart.ko
+
+# Change permissions on litepcie created devices.
+for i in `seq 0 16` ; do
+    chmod 666 /dev/litepcie$i > /dev/null 2>&1
+done
+
