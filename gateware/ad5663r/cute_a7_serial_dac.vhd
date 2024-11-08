bfb8568ace3926ce69d5836ef20db0b282cf3de6
Florent Kermarrec <florent@enjoy-digital.fr>
2024-11-08 15:21:21 +0100

    gateware: Add ad5663r Serial-DAC Core from Cute-A7.

---
+++ gateware/ad5663r/cute_a7_serial_dac.vhd
@@ -0,0 +1,183 @@
+-------------------------------------------------------------------------------
+-- title      : serial dac interface
+-- project    : white rabbit switch
+-------------------------------------------------------------------------------
+-- file       : serial_dac.vhd
+-- author     : paas, slayer
+-- company    : cern be-co-ht
+-- created    : 2010-02-25
+-- last update: 2011-05-10
+-- platform   : fpga-generic
+-- standard   : vhdl'87
+-------------------------------------------------------------------------------
+-- description: the dac unit provides an interface to a 16 bit serial digita to analogue converter (max5441, spi?/qspi?/microwire? compatible) 
+--
+-------------------------------------------------------------------------------
+-- copyright (c) 2010 cern
+-------------------------------------------------------------------------------
+-- revisions  :1
+-- date        version  author  description
+-- 2009-01-24  1.0      paas    created
+-- 2010-02-25  1.1      slayer  modified for rev 1.1 switch
+-- 2012-10-15  2.0      pwb     modified for ad5663r of cute-wr
+-------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cute_a7_serial_dac is
+
+  generic (
+    g_num_data_bits  : integer := 16;
+    g_num_extra_bits : integer := 8
+    );
+
+  port (
+-- clock & reset
+    clk_i   : in std_logic;
+    rst_n_i : in std_logic;
+
+-- channel 1 value and value load strobe
+    value_i  : in std_logic_vector(g_num_extra_bits+g_num_data_bits-1 downto 0);
+    load_i   : in std_logic;
+
+-- sclk divider: 000 = clk_i/8 ... 111 = clk_i/1024
+    sclk_divsel_i : in std_logic_vector(2 downto 0);
+
+-- dac i/f
+    plldac_sclk_o   : out std_logic;
+    plldac_sdata_o  : out std_logic;
+    plldac_sync_n_o : out std_logic;   
+
+    xdone_o : out std_logic
+    );
+end cute_a7_serial_dac;
+
+
+architecture syn of cute_a7_serial_dac is
+
+  signal divider        : unsigned(11 downto 0);
+  signal datash         : std_logic_vector(g_num_data_bits + g_num_extra_bits-1 downto 0);
+  signal bitcounter     : std_logic_vector(g_num_data_bits + g_num_extra_bits+1 downto 0);
+  signal endsendingdata : std_logic;
+  signal sendingdata    : std_logic;
+  signal idacclk        : std_logic;
+  signal ivalidvalue    : std_logic;
+
+  signal divider_muxed : std_logic;
+
+begin
+
+  -- modified by weibin
+  
+  select_divider : process (divider, sclk_divsel_i)
+  begin  -- process
+    case sclk_divsel_i is
+      when "000"  => divider_muxed <= divider(2);  -- sclk = clk_i/8
+      when "001"  => divider_muxed <= divider(3);  -- sclk = clk_i/16
+      when "010"  => divider_muxed <= divider(4);  -- sclk = clk_i/32
+      when "011"  => divider_muxed <= divider(5);  -- sclk = clk_i/64
+      when "100"  => divider_muxed <= divider(6);  -- sclk = clk_i/128
+      when "101"  => divider_muxed <= divider(7);  -- sclk = clk_i/256
+      when "110"  => divider_muxed <= divider(8);  -- sclk = clk_i/512
+      when "111"  => divider_muxed <= divider(9);  -- sclk = clk_i/1024
+      when others => null;
+    end case;
+  end process;
+
+
+  ivalidvalue <= load_i;
+
+  process(clk_i, rst_n_i)
+  begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        sendingdata <= '0';
+      else
+        if ivalidvalue = '1' and sendingdata = '0' then
+          sendingdata <= '1';
+        elsif endsendingdata = '1' then
+          sendingdata <= '0';
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      if ivalidvalue = '1' then
+        divider <= (divider'high downto 1 => '0') & '1';
+      elsif sendingdata = '1' then
+        if(divider_muxed = '1') then
+          divider <= (divider'high downto 1 => '0') & '1';
+        else
+          divider <= divider + 1;
+        end if;
+      elsif endsendingdata = '1' then
+        divider <= (divider'high downto 1 => '0') & '1';
+      end if;
+    end if;
+  end process;
+
+
+  process(clk_i, rst_n_i)
+  begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        idacclk <= '1';                 -- 0
+      else
+        if ivalidvalue = '1' then
+          idacclk <= '1';               -- 0
+        elsif divider_muxed = '1' then
+          idacclk <= not(idacclk);
+        elsif endsendingdata = '1' then
+          idacclk <= '1';               -- 0
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk_i, rst_n_i)
+  begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        datash <= (others => '0');
+      else
+        if ivalidvalue = '1' and sendingdata = '0' then
+          datash        <= value_i;
+        elsif sendingdata = '1' and divider_muxed = '1' and idacclk = '0' then
+          datash(0)                    <= datash(datash'left);
+          datash(datash'left downto 1) <= datash(datash'left - 1 downto 0);
+        end if;
+      end if;
+    end if;
+  end process;
+
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      if ivalidvalue = '1' and sendingdata = '0' then
+        bitcounter(0)                        <= '1';
+        bitcounter(bitcounter'left downto 1) <= (others => '0');
+      elsif sendingdata = '1' and to_integer(divider) = 1 and idacclk = '1' then
+        bitcounter(0)                        <= '0';
+        bitcounter(bitcounter'left downto 1) <= bitcounter(bitcounter'left - 1 downto 0);
+      end if;
+    end if;
+  end process;
+
+  endsendingdata <= bitcounter(bitcounter'left);
+
+  xdone_o <= not sendingdata;
+
+  plldac_sdata_o <= datash(datash'left);
+  
+  plldac_sync_n_o <= not sendingdata;
+
+  plldac_sclk_o <= idacclk;
+
+
+end syn;
---
+++ gateware/ad5663r/cute_a7_serial_dac_arb.vhd
@@ -0,0 +1,140 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity cute_a7_serial_dac_arb is
+  generic(
+    g_invert_sclk    : boolean;
+    g_num_data_bits  : integer:=16;
+    g_num_extra_bits : integer:=8
+    );
+  port(
+    clk_i   : in std_logic;
+    rst_n_i : in std_logic;
+
+    val_i  : in std_logic_vector(g_num_data_bits-1 downto 0);
+    load_i : in std_logic;
+
+    dac_ldac_n_o    : out std_logic;
+    dac_clr_n_o     : out std_logic;
+    dac_sync_n_o    : out std_logic;
+    dac_sclk_o      : out std_logic;
+    dac_din_o       : out std_logic);
+
+end cute_a7_serial_dac_arb;
+
+architecture behavioral of cute_a7_serial_dac_arb is
+
+  component cute_a7_serial_dac
+    generic (
+      g_num_data_bits  : integer;
+      g_num_extra_bits : integer);
+    port (
+      clk_i          : in  std_logic;
+      rst_n_i        : in  std_logic;
+      value_i        : in  std_logic_vector(g_num_extra_bits+g_num_data_bits-1 downto 0);
+      load_i         : in  std_logic;
+      sclk_divsel_i  : in  std_logic_vector(2 downto 0);
+      plldac_sclk_o  : out std_logic;
+      plldac_sdata_o : out std_logic;
+      plldac_sync_n_o: out std_logic;
+      xdone_o        : out std_logic);
+  end component;
+
+  signal d1_ready     : std_logic;
+  signal dac_data     : std_logic_vector(g_num_data_bits-1 downto 0);
+  signal dac_cmd      : std_logic_vector(g_num_extra_bits-1 downto 0);
+  signal dac_value    : std_logic_vector(g_num_data_bits+g_num_extra_bits-1 downto 0);
+  signal dac_load     : std_logic;
+  signal dac_cs_sel   : std_logic_vector(1 downto 0);
+  signal dac_done     : std_logic;
+  signal dac_sclk_int : std_logic;
+
+  signal init_cnt : unsigned(7 downto 0);
+
+  type t_state is (INIT, ENABLE_INT_REF, WAIT_DONE, LOAD_DAC, WAIT_DATA);
+  signal state : t_state;
+
+begin  -- behavioral
+
+  dac_ldac_n_o  <= '0';
+  dac_clr_n_o   <= '0';
+  
+  U_DAC : cute_a7_serial_dac
+    generic map (
+      g_num_data_bits  => 16,
+      g_num_extra_bits => g_num_extra_bits)
+    port map (
+      clk_i         => clk_i,
+      rst_n_i       => rst_n_i,
+      value_i       => dac_value,   
+      load_i        => dac_load,
+      sclk_divsel_i => "001",
+      plldac_sclk_o   => dac_sclk_int,
+      plldac_sdata_o  => dac_din_o,
+      plldac_sync_n_o => dac_sync_n_o,
+      xdone_o         => dac_done);
+
+  p_drive_sclk: process(dac_sclk_int)
+    begin
+      if(g_invert_sclk) then
+        dac_sclk_o <= not dac_sclk_int;
+       else
+        dac_sclk_o <= dac_sclk_int;
+       end if;
+      end process;
+
+  process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      if rst_n_i = '0' then
+        dac_data   <= (others => '0');
+        d1_ready   <= '0';
+        dac_load   <= '0';
+        init_cnt   <= (others => '0');
+        state      <= INIT;
+      else
+
+        if(load_i = '1') then
+          if(load_i = '1') then
+            d1_ready <= '1';
+            dac_data <= val_i;
+          end if;
+        else
+          case state is
+            when INIT => 
+              if init_cnt(6) = '1' then
+                state <= ENABLE_INT_REF;
+              else
+                init_cnt <= init_cnt +1;
+              end if;
+
+            when ENABLE_INT_REF =>
+              dac_value <= "0100"&"000000"&"00000000000000";  -- Enable internal reference
+              dac_load  <= '1';
+              state <= LOAD_DAC;
+              
+            when WAIT_DATA =>
+              if(d1_ready = '1') then
+                dac_value   <= "0011"&dac_data&"0000";
+                dac_load   <= '1';
+                d1_ready   <= '0';
+                state      <= LOAD_DAC;
+              end if;
+
+            when LOAD_DAC=>
+              dac_load <= '0';
+              state    <= WAIT_DONE;
+
+            when WAIT_DONE =>
+              if(dac_done = '1') then
+                state <= WAIT_DATA;
+              end if;
+            when others => null;
+          end case;
+        end if;
+      end if;
+    end if;
+  end process;
+  
+end behavioral;
