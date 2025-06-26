5e43504452d76f413cdc90bc899a8b67126f4f0d
Florent Kermarrec <florent@enjoy-digital.fr>
2025-06-26 10:22:20 +0200

    gateware/soc: Add add_wr_core method to ease integration of WR Core on targets.

--- gateware/soc.py
+++ gateware/soc.py
@@ -11,12 +11,14 @@ import sys
 
 from migen import *
 
-from litex.build import tools
+from litex.build    import tools
+from litex.build.io import SDRTristate, SDROutput
 
 from litex.gen import *
 
 from litex.soc.interconnect import wishbone
 
+from litex.soc.integration.soc      import SoCRegion
 from litex.soc.integration.soc_core import *
 
 from litescope import LiteScopeAnalyzer
@@ -28,7 +30,10 @@ from gateware.nic import sram
 sys.modules["liteeth.mac.sram"] = sram #  Replace Liteeth SRAM with our custom implementation.
 from gateware.nic.dma import LitePCIe2WishboneDMA
 
-from gateware.wr_common import wr_core_init, wr_core_files
+from gateware.wr_common         import wr_core_init, wr_core_files
+from gateware.wrf_stream2wb     import Stream2Wishbone
+from gateware.wrf_wb2stream     import Wishbone2Stream
+from gateware.wb_clock_crossing import WishboneClockCrossing
 
 # LiteX WR NIC SoC ---------------------------------------------------------------------------------
 
@@ -70,6 +75,219 @@ class LiteXWRNICSoC(SoCMini):
         "ethmac0"  : 0x0002_0000,
         "ethmac1"  : 0x0004_0000,
     }
+
+    # Add White Rabbit Core ------------------------------------------------------------------------
+    def add_wr_core(self,
+        # CPU.
+        cpu_firmware,
+
+        # SFP.
+        sfp_pads,
+        sfp_i2c_pads,
+        sfp_tx_polarity  = 0,
+        sfp_rx_polarity  = 0,
+        sfp_disable_pads = None,
+        sfp_fault_pads   = None,
+        sfp_los_pads     = None,
+        sfp_det_pads     = None,
+
+        # Clocking.
+        qpll         = None,
+        with_ext_clk = True,
+
+        # Serial.
+        serial_pads = None,
+
+        # Flash.
+        flash_pads = None,
+
+        # Temp 1Wire.
+        temp_1wire_pads = None,
+    ):
+
+        # Clks.
+        # -----
+        self.cd_wr = ClockDomain("wr")
+
+        # Signals.
+        # --------
+        self.led_pps         = Signal()
+        self.led_link        = Signal()
+        self.led_act         = Signal()
+        self.dac_refclk_load = Signal()
+        self.dac_refclk_data = Signal(16)
+        self.dac_dmtd_load   = Signal()
+        self.dac_dmtd_data   = Signal(16)
+        self.pps             = Signal()
+        self.pps_in          = Signal()
+        self.pps_out_valid   = Signal()
+        self.pps_out         = Signal()
+        self.pps_out_pulse   = Signal()
+        self.tm_link_up      = Signal()
+        self.tm_time_valid   = Signal()
+        self.tm_seconds      = Signal(40)
+        self.tm_cycles       = Signal(28)
+
+        # White Rabbit Fabric Interface.
+        # ------------------------------
+        self.wrf_stream2wb = wrf_stream2wb = Stream2Wishbone(  cd_to="wr")
+        self.wrf_wb2stream = wrf_wb2stream = Wishbone2Stream(cd_from="wr")
+
+        # White Rabbit Slave Interface.
+        # -----------------------------
+        self.wb_slave_sys = wb_slave_sys = wishbone.Interface(data_width=32, address_width=32, adressing="byte")
+        self.wb_slave_wr  = wb_slave_wr  = wishbone.Interface(data_width=32, address_width=32, adressing="byte")
+        self.bus.add_slave(name="wr_wb_slave", slave=wb_slave_sys, region=SoCRegion(
+             origin = 0x2000_0000,
+             size   = 0x0100_0000,
+         ))
+        self.submodules += WishboneClockCrossing(self.platform,
+            wb_from = wb_slave_sys,
+            cd_from = "sys",
+            wb_to   = wb_slave_wr,
+            cd_to   = "wr",
+        )
+
+        # Temp 1-Wire Logic.
+        # ------------------
+        if temp_1wire_pads is not None:
+            temp_1wire_oe_n = Signal()
+            temp_1wire_i    = Signal()
+            self.specials += SDRTristate(
+                io = temp_1wire_pads,
+                o  = Constant(0b0, 1),
+                oe = ~temp_1wire_oe_n,
+                i  = temp_1wire_i,
+            )
+
+        # Flash Logic.
+        # ------------
+        if flash_pads is not None:
+            flash_clk = Signal()
+            self.specials += Instance("STARTUPE2",
+                i_CLK       = 0,
+                i_GSR       = 0,
+                i_GTS       = 0,
+                i_KEYCLEARB = 0,
+                i_PACK      = 0,
+                i_USRCCLKO  = flash_clk,
+                i_USRCCLKTS = 0,
+                i_USRDONEO  = 1,
+                i_USRDONETS = 1,
+            )
+
+        # White Rabbit Core Instance.
+        # ---------------------------
+        self.specials += Instance("xwrc_board_spec_a7_wrapper",
+            # Parameters.
+            p_g_dpram_initf               = os.path.abspath(cpu_firmware),
+            p_g_dpram_size                = 131072//4,
+            p_txpolarity                  = sfp_tx_polarity,
+            p_rxpolarity                  = sfp_rx_polarity,
+            p_g_with_external_clock_input = str(with_ext_clk).upper(),
+
+            # Clocks/resets.
+            i_areset_n_i          = ~ResetSignal("sys"),
+            i_clk_62m5_dmtd_i     = ClockSignal("clk_62m5_dmtd"),
+            i_clk_125m_gtp_i      = ClockSignal("clk_125m_gtp"),
+            i_clk_10m_ext_i       = ClockSignal("clk10m_in"),
+            o_clk_62m5_sys_o      = ClockSignal("wr"),
+            o_rst_62m5_sys_o      = ResetSignal("wr"),
+
+            # DAC RefClk Interface.
+            o_dac_refclk_load     = self.dac_refclk_load,
+            o_dac_refclk_data     = self.dac_refclk_data,
+
+            # DAC DMTD Interface.
+            o_dac_dmtd_load       = self.dac_dmtd_load,
+            o_dac_dmtd_data       = self.dac_dmtd_data,
+
+            # SFP Interface.
+            o_sfp_txp_o           = sfp_pads.txp,
+            o_sfp_txn_o           = sfp_pads.txn,
+            i_sfp_rxp_i           = sfp_pads.rxp,
+            i_sfp_rxn_i           = sfp_pads.rxn,
+            i_sfp_det_i           = 0      if sfp_det_pads is None else sfp_det_pads,
+            io_sfp_sda            = sfp_i2c_pads.sda,
+            io_sfp_scl            = sfp_i2c_pads.scl,
+            i_sfp_tx_fault_i      = 0      if   sfp_fault_pads is None else   sfp_fault_pads,
+            i_sfp_tx_los_i        = 0      if     sfp_los_pads is None else     sfp_los_pads,
+            o_sfp_tx_disable_o    = Open() if sfp_disable_pads is None else sfp_disable_pads,
+
+            # One-Wire Interface.
+            i_onewire_i           = 0      if temp_1wire_pads is None else temp_1wire_i,
+            o_onewire_oen_o       = Open() if temp_1wire_pads is None else temp_1wire_oe_n,
+
+            # UART Interface.
+            i_uart_rxd_i          = 0      if serial_pads is None else serial_pads.rx,
+            o_uart_txd_o          = Open() if serial_pads is None else serial_pads.tx,
+
+            # SPI Flash Interface.
+            o_spi_sclk_o          = Open() if flash_pads is None else flash_clk,
+            o_spi_ncs_o           = Open() if flash_pads is None else flash_pads.cs_n,
+            o_spi_mosi_o          = Open() if flash_pads is None else flash_pads.mosi,
+            i_spi_miso_i          = 0      if flash_pads is None else flash_pads.miso,
+
+            # PPS / Leds.
+            o_pps_valid_o         = Open(),
+            i_pps_ext_i           = self.pps_in,
+            o_pps_csync_o         = self.pps_out_pulse,
+            o_pps_p_o             = self.pps,
+            o_pps_led_o           = self.led_pps,
+            o_led_link_o          = self.led_link,
+            o_led_act_o           = self.led_act,
+
+            # QPLL Interface (for GTPE2_Common Sharing).
+            o_gt0_ext_qpll_reset  = Open() if qpll is None else qpll.get_channel("eth").reset,
+            i_gt0_ext_qpll_clk    = 0      if qpll is None else qpll.get_channel("eth").clk,
+            i_gt0_ext_qpll_refclk = 0      if qpll is None else qpll.get_channel("eth").refclk,
+            i_gt0_ext_qpll_lock   = 0      if qpll is None else qpll.get_channel("eth").lock,
+
+            # Wishbone Slave Interface (MMAP).
+            i_wb_slave_cyc        = wb_slave_wr.cyc,
+            i_wb_slave_stb        = wb_slave_wr.stb,
+            i_wb_slave_we         = wb_slave_wr.we,
+            i_wb_slave_adr        = Cat(Signal(2), (wb_slave_wr.adr & 0x00ff_ffff)),
+            i_wb_slave_sel        = wb_slave_wr.sel,
+            i_wb_slave_dat_i      = wb_slave_wr.dat_w,
+            o_wb_slave_dat_o      = wb_slave_wr.dat_r,
+            o_wb_slave_ack        = wb_slave_wr.ack,
+            o_wb_slave_err        = wb_slave_wr.err,
+            o_wb_slave_rty        = Open(),
+            o_wb_slave_stall      = Open(),
+
+            # Wishbone Fabric Source Interface.
+            o_wrf_src_adr         = wrf_wb2stream.bus.adr,
+            o_wrf_src_dat         = wrf_wb2stream.bus.dat_w,
+            o_wrf_src_cyc         = wrf_wb2stream.bus.cyc,
+            o_wrf_src_stb         = wrf_wb2stream.bus.stb,
+            o_wrf_src_we          = wrf_wb2stream.bus.we,
+            o_wrf_src_sel         = wrf_wb2stream.bus.sel,
+
+            i_wrf_src_ack         = wrf_wb2stream.bus.ack,
+            i_wrf_src_stall       = 0, # Not Used.
+            i_wrf_src_err         = wrf_wb2stream.bus.err,
+            i_wrf_src_rty         = 0, # Not Used.
+
+            # Wishbone Fabric Sink Interface.
+            i_wrf_snk_adr         = wrf_stream2wb.bus.adr,
+            i_wrf_snk_dat         = wrf_stream2wb.bus.dat_w,
+            i_wrf_snk_cyc         = wrf_stream2wb.bus.cyc,
+            i_wrf_snk_stb         = wrf_stream2wb.bus.stb,
+            i_wrf_snk_we          = wrf_stream2wb.bus.we,
+            i_wrf_snk_sel         = wrf_stream2wb.bus.sel,
+
+            o_wrf_snk_ack         = wrf_stream2wb.bus.ack,
+            o_wrf_snk_stall       = Open(), # Not Used.
+            o_wrf_snk_err         = wrf_stream2wb.bus.err,
+            o_wrf_snk_rty         = Open(), # Not Used.
+
+            # Time.
+            o_tm_link_up_o        = self.tm_link_up,
+            o_tm_time_valid_o     = self.tm_time_valid,
+            o_tm_tai_o            = self.tm_seconds,
+            o_tm_cycles_o         = self.tm_cycles,
+        )
 
     # Add PCIe NIC ---------------------------------------------------------------------------------
 
